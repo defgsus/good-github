c62122d01f367c9ed77afad1de073ecea3908a73
bergi <s.berke@netzkolchose.de>
2021-12-17 18:26:32 +0100

    add commandline switch to analyze a month or year at once

--- run.py
+++ run.py
@@ -3,11 +3,13 @@ import datetime
 import json
 from pathlib import Path
 import os
+from typing import List
 
 from tqdm import tqdm
 
 from src.gharchive import GHArchive
 from src.good_messages import GoodMessages
+from src.update_index import update_index
 
 
 ROOT_DIR = Path(__file__).resolve().parent
@@ -17,8 +19,8 @@ def parse_args() -> dict:
     parser = argparse.ArgumentParser()
     
     parser.add_argument(
-        "day", type=str, 
-        help="Day to inspect, either YYYY-MM-DD or 'yesterday'"
+        "days", type=str, nargs="+",
+        help="Days to scan, either YYYY[-MM[-DD]] or 'yesterday'"
     )
     parser.add_argument(
         "path", type=str,
@@ -30,17 +32,35 @@ def parse_args() -> dict:
     )
     
     args = vars(parser.parse_args())
-    
-    if args["day"] == "yesterday":
-        args["day"] = datetime.date.today() - datetime.timedelta(days=1)
-    else:
-        args["day"] = datetime.datetime.strptime(args["day"], "%Y-%m-%d")
-    
+
+    days = set()
+    for day in args["days"]:
+        if day == "yesterday":
+            days.add(datetime.date.today() - datetime.timedelta(days=1))
+        else:
+            if len(day) == 10:
+                days.add(datetime.datetime.strptime(day, "%Y-%m-%d"))
+            elif len(day) == 7:
+                date = datetime.datetime.strptime(day, "%Y-%m")
+                month = date.month
+                while date.month == month:
+                    days.add(date)
+                    date += datetime.timedelta(days=1)
+            elif len(day) == 4:
+                date = datetime.datetime.strptime(day, "%Y-%m")
+                year = date.year
+                while date.year == year:
+                    days.add(date)
+                    date += datetime.timedelta(days=1)
+            else:
+                raise ValueError(f"Invalid date '{day}'")
+
+    args["days"] = sorted(days)
     return args
 
 
 def main(
-        day: datetime.date,
+        days: List[datetime.date],
         path: str,
         verbose: bool,
 ):
@@ -48,11 +68,11 @@ def main(
         raw_path=path,
         verbose=verbose,
     )
-    proc = GoodMessages(verbose=verbose)
+    for day in days:
+        proc = GoodMessages(verbose=verbose)
 
-    stash_file = (ROOT_DIR / "stash" / str(day)[:4] / f"{str(day)[:10]}.json")
+        stash_file = (ROOT_DIR / "stash" / str(day)[:4] / f"{str(day)[:10]}.json")
 
-    if 1:
         try:
             iterable = archive.iter_events(day)
             if verbose:
@@ -73,22 +93,21 @@ def main(
         proc.store_stash(stash_file)
         proc.store_words(stash_file.parent / f"{str(day)[:10]}-words.json")
 
-    else:
-        proc.load_stash(stash_file)
+        proc.remove_duplicates()
 
-    proc.remove_duplicates()
+        if verbose:
+            print(len(proc.commits), "commits")
 
-    if verbose:
-        print(len(proc.commits), "commits")
+        md_file = (ROOT_DIR / "docs" / "good-messages" / str(day)[:4] / f"{str(day)[:10]}.md")
+        if not md_file.parent.exists():
+            os.makedirs(md_file.parent)
+        md_file.write_text(
+            f"# {str(day)[:10]}\n\n"
+            + proc.render_stats_markdown() + "\n\n"
+            + proc.render_markdown()
+        )
 
-    md_file = (ROOT_DIR / "docs" / str(day)[:4] / f"{str(day)[:10]}.md")
-    if not md_file.parent.exists():
-        os.makedirs(md_file.parent)
-    md_file.write_text(
-        f"# *good github* {str(day)[:10]}\n\n"
-        + proc.render_stats_markdown() + "\n\n"
-        + proc.render_markdown()
-    )
+    update_index()
 
 
 if __name__ == "__main__":
--- src/update_index.py
+++ src/update_index.py
@@ -1,8 +1,38 @@
 import glob
+import datetime
 from pathlib import Path
 
 DOCS_PATH = Path(__file__).resolve().parent.parent / "docs"
 
 
 def update_index():
-    pass
+
+    messages_path = DOCS_PATH / "good-messages"
+
+    files = []
+    for filename in glob.glob(str(messages_path / "????" / "????-??-??.md")):
+        filename = Path(filename)
+
+        rel_path = filename.relative_to(DOCS_PATH)
+        name = filename.name
+        date = (name[:4], name[5:7], name[8:10])
+        files.append((date, rel_path))
+
+    files.sort()
+
+    md = ""
+    year = None
+    month = None
+    for date, filename in files:
+        if date[0] != year:
+            year = date[0]
+            md += f"\n\n### {year}\n\n"
+        if date[1] != month:
+            month = date[1]
+            md += f"\n\n#### {datetime.date(2000, int(month.lstrip('0')), 1).strftime('%B')}\n\n"
+
+        md += f"[{date[2]}]({filename}) "
+
+    md += "\n"
+
+    Path(DOCS_PATH / "messages.md").write_text(md)
